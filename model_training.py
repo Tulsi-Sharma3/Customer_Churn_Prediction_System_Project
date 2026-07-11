4723067b95d0637a4dd3fb9496fc8d452e754587
Tulsi Sharma <tulsisharma7069@gmail.com>
2026-07-11 22:55:19 +0530

    readme added

--- model_training.py
+++ model_training.py
@@ -1,15 +1,15 @@
-import mysql.connector
-db = mysql.connector.connect(host="localhost",user="root",password="4567")
-if db:
-    print("connect successfully")
-else:
-    print("not")
-dbCursor = db.cursor()
-dbCursor.execute("create database tulsi;")
-dbCursor.execute("show databases;")
-# %s= placeholder mean
-# executemany() = insert all row in command
-db.commit()
-'''
-import install pandassql
-'''
+# import mysql.connector
+# db = mysql.connector.connect(host="localhost",user="root",password="4567")
+# if db:
+#     print("connect successfully")
+# else:
+#     print("not")
+# dbCursor = db.cursor()
+# dbCursor.execute("create database tulsi;")
+# dbCursor.execute("show databases;")
+# # %s= placeholder mean
+# # executemany() = insert all row in command
+# db.commit()
+# '''
+# import install pandassql
+# '''
